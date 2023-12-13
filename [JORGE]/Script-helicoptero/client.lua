75dae5d51c1352e7e92f81faf0a629c1ad2ffe1d
zThorium <fabian.figueroa@alumnos.uv.cl>
2023-12-13 18:00:02 -0300

    Arreglo Output

--- [JORGE]/Script-helicoptero/client.lua
+++ [JORGE]/Script-helicoptero/client.lua
@@ -2,7 +2,7 @@ function planeEnter ( theVehicle, seat)
     local id = getElementModel ( theVehicle ) -- obtiene el id del vehiculo
     if id == 497 then --detecta si es el maverick
         local vehicleName = getVehicleName ( theVehicle ) -- obteer el nombre del vehiculo
-        outputChatBox ( "Someone has stolen a " .. vehicleName .. "!" ) -- colocar en el chat el nombre del maverick
+        outputChatBox ( "Someone has stolen a " .. vehicleName .. "!",source ) -- colocar en el chat el nombre del maverick
     end
 end
 -- add the event handler to the event
