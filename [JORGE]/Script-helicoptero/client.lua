11e8e2f352749616ab9d06fbda194ded70987f48
zThorium <fabian.figueroa@alumnos.uv.cl>
2023-12-13 20:53:19 -0300

    Se cambio el meta y configuraciones del sonido

--- [JORGE]/Script-helicoptero/client.lua
+++ [JORGE]/Script-helicoptero/client.lua
@@ -1,8 +1,8 @@
 addEvent("sirenStart",true)
 addEventHandler("sirenStart",root,function()
    local sonido = playSound3D("sirenas/sirena1.mp3", 0,0,0, false)
-   setSoundMaxDistance(sonido, 200)
-   setSoundVolume(sonido,0.1)
+   setSoundMaxDistance(sonido, 500)
+   setSoundVolume(sonido,1)
    attachElements(sonido,source, 0, 0, 0)
 end)
 
