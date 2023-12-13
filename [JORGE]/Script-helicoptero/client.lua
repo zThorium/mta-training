b9d8f7b265921bd2e7650557e1d171c0e883477c
Mamayoda <129697705+Mamayoda@users.noreply.github.com>
2023-12-13 17:56:34 -0300

    maverick
    comando para saber el nombre del vehiculo

---
+++ [JORGE]/Script-helicoptero/client.lua
@@ -0,0 +1,9 @@
+function planeEnter ( theVehicle, seat) 
+    local id = getElementModel ( theVehicle ) -- obtiene el id del vehiculo
+    if id == 497 then --detecta si es el maverick
+        local vehicleName = getVehicleName ( theVehicle ) -- obteer el nombre del vehiculo
+        outputChatBox ( "Someone has stolen a " .. vehicleName .. "!" ) -- colocar en el chat el nombre del maverick
+    end
+end
+-- add the event handler to the event
+addEventHandler ( "onPlayerVehicleEnter", root, planeEnter )
