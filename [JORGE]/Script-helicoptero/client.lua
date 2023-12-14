ef8c9f781391fd28da60e2060ff1a98c66fc1aea
Mamayoda <129697705+Mamayoda@users.noreply.github.com>
2023-12-13 23:26:07 -0300

    agregar los 15 temas

--- [JORGE]/Script-helicoptero/client.lua
+++ [JORGE]/Script-helicoptero/client.lua
@@ -1,7 +1,8 @@
 addEvent("sirenStart",true)
-sirenas = {"sirenas/sirena1.mp3","sirenas/sirena2.mp3","sirenas/sirena3.mp3"}
+sirenas = {"sirenas/sirena1.mp3","sirenas/sirena2.mp3","sirenas/sirena3.mp3","sirenas/sirena4.mp3","sirenas/sirena5.mp3","sirenas/sirena6.mp3","sirenas/sirena7.mp3",
+"sirenas/sirena8.mp3","sirenas/sirena9.mp3","sirenas/sirena10.mp3","sirenas/sirena11.mp3","sirenas/sirena12.mp3","sirenas/sirena13.mp3","sirenas/sirena14.mp3"}
 addEventHandler("sirenStart",root,function()
-   local random = math.random(1,3)
+   local random = math.random(1,14)
    local sonido = playSound3D(sirenas[random], 0,0,0, false)
    setSoundMaxDistance(sonido, 500)
    setSoundVolume(sonido,1)
